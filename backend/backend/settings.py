de87eb3d4429ed580bfbad6fe5294609fc811cca
Lima82 <marina.a.orlenko@gmail.com>
2026-03-19 22:46:18 +0400

    Fix settings.py & main.yml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY', default='my-secret-key-for-dev-only')
+# SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-DEBUG = False
+# DEBUG = False
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 ALLOWED_HOSTS = ['localhost', '158.160.222.87', '127.0.0.1', 'orlm82.ddns.net']
